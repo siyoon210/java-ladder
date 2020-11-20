b7bb15c31219b025879649622eeb690e8363e7a7
siyoon <siyoon210@gmail.com>
2020-11-20 10:09:58 +0900

    refactor: domain패키지 생성, 해당되는 클래스 이동

--- src/main/java/nextstep/ladder/domain/Line.java
+++ src/main/java/nextstep/ladder/domain/Line.java
@@ -1,39 +1,38 @@
 package nextstep.ladder.domain;
 
-import nextstep.ladder.util.pointsgenerator.HalfRandomPointsGenerator;
-import nextstep.ladder.util.pointsgenerator.PointsGenerator;
-
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
 public class Line {
-    public static final String POINTS_INVALID_VALUE_ERR_MSG = "points의 값이 연속된 true일 수 없습니다.";
+    public static final String POINTS_INVALID_VALUE_ERR_MSG = "Points의 값이 연속된 true일 수 없습니다.";
     private final List<Boolean> points;
-
-    private Line(NumberOfParticipants numberOfParticipants, PointsGenerator pointsGenerator) {
-        points = pointsGenerator.getPoints(numberOfParticipants.getPointsSize());
-        validatePoints();
-    }
+    private final Random random = new Random();
 
     private Line(NumberOfParticipants numberOfParticipants) {
-        this(numberOfParticipants, HalfRandomPointsGenerator.getInstance());
+        points = new ArrayList<>(numberOfParticipants.getPointsSize());
+        setPoints(numberOfParticipants);
+        validatePoints();
     }
 
     public static Line from(NumberOfParticipants numberOfParticipants) {
         return new Line(numberOfParticipants);
     }
 
-    public static Line of(NumberOfParticipants numberOfParticipants, PointsGenerator pointsGenerator) {
-        return new Line(numberOfParticipants, pointsGenerator);
-    }
-
     public static Line from(Participants participants) {
         return new Line(participants.getNumberOfParticipants());
     }
 
-    public static Line of(Participants participants, PointsGenerator pointsGenerator) {
-        return new Line(participants.getNumberOfParticipants(), pointsGenerator);
+    private void setPoints(NumberOfParticipants numberOfParticipants) {
+        points.add(random.nextBoolean());
+
+        IntStream.range(1, numberOfParticipants.getPointsSize())
+                .forEach(index -> {
+                    Boolean prevPoint = points.get(index - 1);
+                    points.add(prevPoint ? false : random.nextBoolean());
+                });
     }
 
     private void validatePoints() {
@@ -49,34 +48,7 @@ public class Line {
         return points.size();
     }
 
-    public void pointsForEach(Consumer<Boolean> consumer) {
-        points.forEach(consumer);
-    }
-
-    public int getNextIndexOf(int currentIndex) {
-        if (shouldBeMoveToLeft(currentIndex)) {
-            return currentIndex - 1;
-        }
-
-        if (shouldBeMoveToRight(currentIndex)) {
-            return currentIndex + 1;
-        }
-
-        return currentIndex;
-    }
-
-    private boolean shouldBeMoveToRight(int currentIndex) {
-        return currentIndex < points.size() && points.get(currentIndex);
-    }
-
-    private boolean shouldBeMoveToLeft(int currentIndex) {
-        return currentIndex > 0 && points.get(currentIndex - 1);
-    }
-
-    @Override
-    public String toString() {
-        return "Line{" +
-                "points=" + points +
-                '}';
+    public void repeatAsPoints(Consumer<Boolean> renderPoints) {
+        points.forEach(renderPoints);
     }
 }
--- src/main/java/nextstep/ladder/domain/Participants.java
+++ src/main/java/nextstep/ladder/domain/Participants.java
@@ -1,53 +1,22 @@
 package nextstep.ladder.domain;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class Participants {
-    public static final String DUPLICATE_NAME_EXIST_ERR_MSG = "중복된 이름은 사용 할 수 없습니다.";
     private final List<Name> names;
-    private final NumberOfParticipants numberOfParticipants;
 
-    private Participants(List<String> inputNames) {
-        names = getNames(inputNames);
-        numberOfParticipants = NumberOfParticipants.valueOf(inputNames.size());
-        validateDuplication(inputNames);
-    }
-
-    private List<Name> getNames(List<String> inputNames) {
-        return inputNames.stream()
+    private Participants(List<String> names) {
+        this.names = names.stream()
                 .map(Name::valueOf)
-                .distinct()
                 .collect(Collectors.toList());
-    }
-
-    private void validateDuplication(List<String> inputNames) {
-        if (names.size() != inputNames.size()) {
-            throw new IllegalStateException(DUPLICATE_NAME_EXIST_ERR_MSG);
-        }
     }
 
     public static Participants from(List<String> names) {
         return new Participants(names);
     }
 
-    public static Participants of(String... inputNames) {
-        return new Participants(Arrays.asList(inputNames));
-    }
-
     public NumberOfParticipants getNumberOfParticipants() {
-        return numberOfParticipants;
-    }
-
-    public void namesValueForEach(Consumer<String> consumer) {
-        names.stream()
-                .map(Name::getValue)
-                .forEach(consumer);
-    }
-
-    public Name get(int index) {
-        return names.get(index);
+        return NumberOfParticipants.valueOf(names.size());
     }
 }
